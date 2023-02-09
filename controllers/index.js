262ebc724c42382ec25db091f3e96842f2540f0d
Bram Gibson <Bram.gibson@hotmail.com>
2023-02-09 14:49:45 -0800

    working on getting socket.io to work

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+const roomRoutes = require('./roomController.js');
+router.use("/room",roomRoutes)
 
+const frontEndRoutes = require('./frontEndController');
+router.use("/",frontEndRoutes);
 
 module.exports = router;
